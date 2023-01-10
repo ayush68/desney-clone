21e3fb52b9695e2928e3d0d39cd3362985912c4a
Ayush Sharma <ayush2822002@gmail.com>
2023-01-11 00:17:49 +0530

    second commit

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Home() {
+  return (
+    <Container>
+      Home here
+    </Container>
+  )
+}
+
+export default Home
+
+const Container = styled.main`
+  min-height:calc(100vh-70px);
+  padding: 0 calc(3.5vw+5px);
+  poaition: relative;
+  &:before {
+    background: url("/images/home-background.png") centre centre / cover
+    no-repeat fixed;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`
